6efe70075646a6b3a1e5793edca12d6d2ae95148
unknown <gustavo.aragao@dcomp.ufs.br>
2024-04-27 18:05:13 -0300

    feature: criando a view 'AlertsView' (uma view de testes)

---
+++ src/views/AlertsView.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1 class="text-lg font-black">Toast Notification</h1>
+    <div class="flex gap-2 p-5">
+        <button class="bg-green-400 p-2 rounded">Sucess</button>
+        <button class="bg-red-400 p-2 rounded">Error</button>
+        <button class="bg-yellow-400 p-2 rounded">Invalid</button>
+    </div>
+    
+    <h1 class="text-lg font-black">Base Alert</h1>
+    <div class="flex gap-2 p-5">
+        <button class="bg-green-400 p-2 rounded">Sucess</button>
+        <button class="bg-red-400 p-2 rounded">Error</button>
+        <button class="bg-yellow-400 p-2 rounded">Invalid</button>
+    </div>
+</template>
