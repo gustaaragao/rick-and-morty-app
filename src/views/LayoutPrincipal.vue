83c85a3e7a3b93244e3cc65a95a3b70665776294
Gustavo Henrique <110679992+gustaaragao@users.noreply.github.com>
2024-05-07 11:08:23 -0300

    Feature/RamRouter (#12)
    * feature(ramRouter): get characters
    
    * feature: v-if na div do botão de 'Load More'

--- src/views/LayoutPrincipal.vue
+++ src/views/LayoutPrincipal.vue
@@ -9,23 +9,35 @@
     <!--end: TopBar -->
     <!--begin: Search Input-->
     <div class="flex align-middle justify-center p-8">
-      <SearchInput class="w-[30rem]"> </SearchInput>
+      <SearchInput class="w-[30rem]" @update:model-value="(value) => (searchedCharacter = value)">
+      </SearchInput>
     </div>
     <!--end: Search Input-->
     <!--begin: Section Characters-->
-    <section class="grid grid-cols-3 gap-4 px-10">
+    <section class="grid grid-cols-3 gap-4 px-10 pb-10">
       <div v-for="character in characters" :key="character?.id">
         <VisualizerCharacter class="" :character="character"> </VisualizerCharacter>
       </div>
     </section>
     <!--end: Section Characters-->
+    <!-- begin: Load More Button -->
+    <div v-if="!!nextPageLink">
+      <div class="flex justify-center pb-8">
+        <BaseButton @click="loadNextPage(searchedCharacter, nextPageLink)">
+          <template #text> Load More </template>
+        </BaseButton>
+      </div>
+    </div>
+    <!-- end:  Load More Button -->
   </div>
 </template>
 
 <script setup>
-import axios from 'axios'
-import { onUpdated, ref } from 'vue'
+import { onMounted, onUpdated, ref, watch } from 'vue'
 
+import { ramRouter } from '@/services/api/routing/routers/ramRouter'
+
+import BaseButton from '@/components/buttons/BaseButton.vue'
 import SearchInput from '@/components/inputs/SearchInput.vue'
 import VisualizerCharacter from '@/components/visualizer/VisualizerCharacter.vue'
 import TopBar from '@/components/TopBar.vue'
@@ -34,13 +46,34 @@ const characters = ref([])
 
 const searchedCharacter = ref('')
 
-axios.get(`character?name=${searchedCharacter.value}`).then((response) => {
-  characters.value = response.data.results
-})
+const nextPageLink = ref('')
 
-onUpdated(() => {
-  axios.get(`character?name=${searchedCharacter.value}`).then((response) => {
+onMounted(() => {
+  ramRouter.characters.get().then((response) => {
     characters.value = response.data.results
+
+    nextPageLink.value = response.data.info.next
   })
 })
+
+// onUpdated(() => {
+//   ramRouter.characters.get(name).then((response) => {
+//     characters.value = response.data.results
+
+//     nextPageLink.value = response.data.info.next
+//   })
+// })
+
+const loadNextPage = (searchedCharacter, nextPageLink) => {
+  if (nextPageLink) {
+    const numberPage = nextPageLink.match(/\?page=(\d+)/)[1]
+
+    ramRouter.characters
+      .loadNextPage(searchedCharacter, numberPage)
+      .then((response) => {
+        characters.value.push(...response.data.results)
+        nextPageLink = response.data.info.next
+      })
+  }
+}
 </script>
