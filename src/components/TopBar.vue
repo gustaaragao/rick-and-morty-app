f974b4a0d2fce43d4e13f466cbdb6beef603431c
Gustavo Henrique <gustavohenriquearagaosilva@gmail.com>
2024-05-09 10:59:03 -0300

    feature: Dropdown

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,19 +1,36 @@
 <template>
   <!--begin: TopBar-->
-  <div
-    class="w-full h-20 py-1 px-8 rounded-b-2xl bg-green-400 shadow-md flex justify-between items-center z-50"
-  >
-    <div>Olá, Gustavo</div>
-    <!--begin: Image-->
+  <div class="w-full h-20 py-1 px-4 rounded-b-2xl bg-green-400 shadow-md flex justify-between items-center z-50">
+    <!-- begin: Dropdown User -->
+    <div class="flex gap-2">
+      <Dropdown>
+        <button @click="console.log('Profile')"> Profile </button>
+        <button @click="console.log('Logout')"> Logout </button>
+      </Dropdown>
+      <!-- begin: User Image -->
+      <!-- <div class="scale-[0.02]">
+        <img class="object-scale-down" src="../../db/images/jiji.jpg">
+      </div> -->
+      <!-- end: User Image -->
+      <!-- begin: User Name -->
+      <span>Gustavo</span>
+      <!-- end: User Name -->
+    </div>
+    <!-- end: Dropdown User -->
+    <!--begin: Logo -->
     <div class="w-60">
       <slot name="image"></slot>
     </div>
-    <!--end: Image-->
+    <!--end: Logo-->
     <!--begin: ToggleSwitch-->
     <div>
       <ToggleSwitch>
-        <template #iconOn> <Sun size="16" /> </template>
-        <template #iconOff> <Moon size="16" /> </template>
+        <template #iconOn>
+          <Sun size="16" />
+        </template>
+        <template #iconOff>
+          <Moon size="16" />
+        </template>
       </ToggleSwitch>
     </div>
     <!--end: ToggleSwitch-->
@@ -23,6 +40,15 @@
 
 <script setup>
 import ToggleSwitch from './buttons/ToggleSwitch.vue'
-
+import Dropdown from './dropdown/Dropdown.vue'
 import { Moon, Sun } from 'lucide-vue-next'
+
+const acessProfileUser = () => {
+
+}
+
+const logoutUser = () => {
+
+}
+
 </script>
---
+++ src/components/dropdown/Dropdown.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cursor-pointer">
+    <!-- begin: Dropdown Icon -->
+    <div @click="showDropdown = !showDropdown">
+      <i v-if="showDropdown"><ChevronDown /></i>
+      <i v-else><ChevronUp /></i>
+    </div>
+    <!-- begin: Options Dropdown -->
+    <div class="absolute" v-show="showDropdown">
+      <div class="grid gap-1 p-1 mt-2 border-2 rounded-md divide-y divide-green-700 border-green-700 bg-green-300">
+        <slot></slot>
+      </div>
+    </div>
+    <!-- end: Options Dropdown -->
+    <!-- end: Dropdown Icon -->
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { ChevronDown, ChevronUp } from 'lucide-vue-next';
+
+const showDropdown = ref(false)
+
+</script>
