8d71b37880023ccb3095b3952db0a3a10a391fdb
Gustavo Henrique <gustavohenriquearagaosilva@gmail.com>
2024-05-13 11:37:08 -0300

    style(Visualizer Character): adicionando status

--- src/components/visualizer/VisualizerCharacter.vue
+++ src/components/visualizer/VisualizerCharacter.vue
@@ -1,17 +1,20 @@
 <template>
   <!--begin: Visualizer Character-->
-  <div class="w-full h-full rounded-xl bg-green-300 shadow-2xl">
+  <div class="w-full h-full rounded-xl shadow-2xl border-4 border-gray-300">
     <div class="w-full">
+      <!-- begin: Status -->
+      <div class="absolute mt-4 ml-2">
+        <div class="p-0.5 rounded-md text-white font-medium" :class="colorsStatusCharacter[character.status]">
+          {{ character.status.toUpperCase() }}
+        </div>
+      </div>
+      <!-- end: Status -->
       <!--begin: Favorite Button-->
-      <div
-        class="flex justify-end cursor-pointer"
-        @click="
-          () => {
-            fillIcon = !fillIcon
-            emit('send:character', { id: props.character.id, name: props.character.name })
-          }
-        "
-      >
+      <div class="flex justify-end cursor-pointer" @click="() => {
+          fillIcon = !fillIcon
+          emit('send:character', { id: props.character.id, name: props.character.name })
+        }
+        ">
         <div class="absolute mt-4 mr-4 z-0">
           <Star :class="fillIcon ? 'fill-blue-500' : ''" class="scale-[1.5] stroke-[1.5px]" />
         </div>
@@ -20,14 +23,15 @@
 
       <!--begin: Character Image-->
       <div class="w-full">
-        <img :src="character.image" class="rounded-t-xl" />
+        <img :src="character.image" class="rounded-t-lg" />
       </div>
       <!--end: Character Image-->
 
       <!--begin: Section Character Data-->
-      <section class="grid gap-2 pl-2 pb-1 text-lg rounded-b-xl">
-        <header class="text-xl">{{ character.name }}</header>
-        <p><b>Status:</b> {{ character.status }}</p>
+      <section class="grid gap-2 pl-2 py-1 text-lg rounded-b-xl">
+        <header class="font-bold text-xl">
+          {{ character.name }}
+        </header>
         <p><b>Species:</b> {{ character.species }}</p>
         <p><b>Gender:</b> {{ character.gender }}</p>
       </section>
@@ -51,4 +55,10 @@ const props = defineProps({
 })
 
 const emit = defineEmits(['send:character'])
+
+const colorsStatusCharacter = {
+  'Alive': 'bg-green-400',
+  'Dead': 'bg-red-400',
+  'unknown': 'bg-gray-400',
+}
 </script>
