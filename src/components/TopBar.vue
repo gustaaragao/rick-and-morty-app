801b45ea972ea622681c794349722fcebffceb0e
Gustavo Henrique <gustavohenriquearagaosilva@gmail.com>
2024-05-14 09:10:26 -0300

    styles(TopBar): mudança das cores das letras da TopBar

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -3,21 +3,23 @@
   <div class="w-full h-20 py-1 px-4 rounded-b-2xl bg-green-400 shadow-md flex justify-between items-center z-50">
     <div class="flex gap-1">
       <!-- begin: LogOut Button -->
-      <div class="cursor-pointer pl-2" @click="logoutUser">
+      <div class="cursor-pointer pl-2 text-slate-100" @click="logoutUser">
         <LogOut />
       </div>
       <!-- end: LogOut Button -->
       <!-- begin: Profile Button -->
-      <div class="cursor-pointer pl-2" @click="router.push('/profile')">
+      <div class="cursor-pointer pl-2 text-slate-100" @click="router.push('/profile')">
         <CircleUserRound />
       </div>
       <!-- end: Profile Button -->
     </div>
-    <!-- begin: Logo -->
-    <div class="w-60">
-      <slot name="image"></slot>
+    <!-- begin: Top Menu -->
+    <div class="flex gap-8 text-slate-100 text-xl font-bold underline select-none">
+      <a class="cursor-pointer">Characters</a>
+      <a class="cursor-pointer">Locations</a>
+      <a class="cursor-pointer">Episodes</a>
     </div>
-    <!-- end: Logo-->
+    <!-- end: Top Menu -->
     <!-- begin: ToggleSwitch-->
     <div>
       <ToggleSwitch>
