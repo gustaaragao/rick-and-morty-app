eb3ef48b6eba93de637e5daf314768f3ed4a77b6
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2024-05-25 20:08:16 -0300

    feature: criando prototipo da tabela

---
+++ src/components/table/Table.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="table-container" class="mx-20">
+    <table class="w-full">
+      <!-- begin: Header  -->
+      <tr>
+        <th v-for="name in computedHeaderNames">
+          {{ name }}
+        </th>
+      </tr>
+      <!-- end: Header -->
+      <!-- begin: Rows -->
+      <tr v-for="row in props.rows">
+        <td v-for="item in Object.values(row)" class="text-center">
+          {{ item }}
+        </td>
+      </tr>
+      <!-- end: Rows -->
+    </table>
+  </div>
+</template>
+
+<script setup>
+import { computed, watch } from 'vue';
+
+const props = defineProps({
+  rows: {
+    type: Object,
+    required: true,
+  },
+})
+
+const computedHeaderNames = computed(() => {
+  return Object.keys(props.rows[0]).map((name) => name.toUpperCase().replace('_', ' '))
+})
+</script>
+
+<style scoped>
+
+#table-container {
+  width: 100%;
+  border: 2px solid rgb(229 231 235);
+  border-radius: 0.5em
+}
+
+
+th,td {
+  border-right: 2px solid rgb(229 231 235);
+}
+
+th:last-child, td:last-child {
+  border-right: 0px;
+}
+
+td {
+  border-top: 2px solid rgb(229 231 235);
+}
+</style>
--- src/views/EpisodesView.vue
+++ src/views/EpisodesView.vue
@@ -1,6 +1,33 @@
 <template>
-Episodes
+  <div class="flex justify-center pt-10">
+    <Table
+    :rows="mockData"
+    />
+  </div>
 </template>
 
 <script setup>
+import Table from '@/components/table/Table.vue'
+
+const mockData = [
+  {
+    episode: '01',
+    name: 'Gustavo',
+    air_date: '27/05/2024',
+    characters: 'AA',
+  },
+  {
+    episode: '02',
+    name: 'Henrique',
+    air_date: '01/01/2024',
+    characters: 'BB',
+  },
+  {
+    episode: '03',
+    name: 'Aragao',
+    air_date: '31/12/2024',
+    characters: 'CC',
+  },
+]
+
 </script>
