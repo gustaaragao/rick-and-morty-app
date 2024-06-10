85524810dbfd817c19a6c239d858fe6061bd0a28
Gustavo Henrique <gustavohenriquearagaosilva@gmail.com>
2024-06-10 10:48:52 -0300

    feature: rota para locations

--- src/services/api/routing/routers/ramRouter.js
+++ src/services/api/routing/routers/ramRouter.js
@@ -1,7 +1,9 @@
 import { characters } from "../routes/ricky-and-morty-api/characters"
 import { episodes } from "../routes/ricky-and-morty-api/episodes"
+import { locations } from "../routes/ricky-and-morty-api/locations"
 
 export const ramRouter = {
     characters,
-    episodes
+    episodes,
+    locations
 }
