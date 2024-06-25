cd2a770669958d55ecdbcad9f1e3a9369a77b37c
Gustavo Henrique <gustavohenriquearagaosilva@gmail.com>
2024-06-25 11:30:40 -0300

    bg-white

--- src/components/visualizer/VisualizerCharacter.vue
+++ src/components/visualizer/VisualizerCharacter.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full h-full rounded-3xl shadow-lg"
+    class="w-full h-full bg-white rounded-3xl shadow-lg"
     :class="designHorizontal ? 'flex' : ''"
   >
     <!-- begin: Character Image -->
