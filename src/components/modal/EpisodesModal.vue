7225404a1c6f6c649692cc8fc9d2286137555180
Gustavo Henrique <gustavohenriquearagaosilva@gmail.com>
2024-06-25 10:36:03 -0300

    feature: criando o EpisodesModal

---
+++ src/components/modal/EpisodesModal.vue
@@ -0,0 +1,28 @@
+<template>
+  <BaseModal>
+    <template #icon>
+      <Tv 
+        size="3em"
+        class="stroke-2"
+      />
+    </template>
+
+    <div v-for="episode in props.episodesUrl">
+      {{ episode }}
+    </div>
+  </BaseModal>
+</template>
+
+<script setup>
+import BaseModal from './BaseModal.vue';
+import { Tv } from 'lucide-vue-next';
+import { ramRouter } from '@/services/api/routing/routers/ramRouter';
+
+const props = defineProps({
+  episodesUrl: {
+    type: Array,
+    required: true
+  }
+})
+
+</script>
