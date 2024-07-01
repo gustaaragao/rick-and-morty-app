b88ee3d73e1a4d4ee6f1f6b7c738892e59097d49
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2024-07-01 17:51:54 -0300

    feature: adicionando mudancas ao locationstable

--- src/components/table/LocationsTable.vue
+++ src/components/table/LocationsTable.vue
@@ -12,28 +12,94 @@
     </template>
     <template #pagination>
       <Pagination 
+        v-if="showPagination"
         :info="locationsInfo"
+        :previous-page="previousPage"
+        :next-page="nextPage"
         @load:previous-page="loadPreviousPage()"
         @load:next-page="loadNextPage()"
+        @load:selected-page="(numberOfPage) => loadSelectedPage(numberOfPage)"
       />
     </template>
   </Table>
 </template>
 
 <script setup>
-import Table from '@/components/table/Table.vue'
+import Table from '@/components/table/Table.vue';
 import SearchTable from './handler/SearchTable.vue';
 import Pagination from './handler/Pagination.vue';
 import { ramRouter } from '@/services/api/routing/routers/ramRouter';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
+import { getPage, replaceNumberOfPage } from '@/utils/pagination';
 
 const locationsData = ref([])
 const locationsInfo = ref({})
 
-onMounted(() => {
-  ramRouter.locations.getAll().then((response) => {
+const showPagination = computed(() => locationsInfo?.value?.pages != 1)
+
+const previousPage = computed(() => getPage(locationsInfo.value.prev))
+const nextPage = computed(() => getPage(locationsInfo.value.next))
+
+const search = (searchObject) => {
+  ramRouter.locations.getByQuery(searchObject.option, searchObject.value)
+  .then((response) => {
     locationsData.value = response.data.results
     locationsInfo.value = response.data.info
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+}
+
+const loadPreviousPage = () => {
+  ramRouter.locations.loadPage(previousPage.value)
+  .then((response) => {
+    locationsData.value = response.data.results
+    locationsInfo.value = response.data.info
+  })
+  .catch((err) => {
+    console.err(err);
+  })
+}
+
+const loadNextPage = () => {
+  ramRouter.locations.loadPage(nextPage.value)
+  .then((response) => {
+    locationsData.value = response.data.results
+    locationsInfo.value = response.data.info
+  })
+  .catch((err) => {
+    console.err(err);
+  })
+}
+
+const loadSelectedPage = (numberOfPage) => {
+  const page = computed(() => {
+    if (previousPage.value) {
+      return replaceNumberOfPage(numberOfPage, previousPage.value)
+    } else {
+      return replaceNumberOfPage(numberOfPage, nextPage.value)
+    }
+  })
+
+  ramRouter.locations.loadPage(page.value)
+  .then((response) => {
+    locationsData.value = response.data.results
+    locationsInfo.value = response.data.info
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+}
+
+onMounted(() => {
+  ramRouter.locations.getAll()
+  .then((response) => {
+    locationsData.value = response.data.results
+    locationsInfo.value = response.data.info
+  })
+  .catch((err) => {
+    console.error(err);
   })
 })
 </script>
