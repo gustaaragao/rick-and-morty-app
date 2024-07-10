b203938987597551e8c1146705d1eec5b9577912
Gustavo Henrique <gustavohenriquearagaosilva@gmail.com>
2024-07-10 11:56:43 -0300

    feat: favorite characters in ProfileView

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,77 +1,81 @@
 <template>
-  <!-- begin: User Data -->
-  <div class="w-1/2 flex flex-col items-center justify-center bg-yellow-400">
-    <!-- begin: Inputs -->
-    <div class="w-2/3 bg-purple-400">
-      <div>
-        <h2 class="py-2"> First Name </h2>
-        <BaseInput
-          placeholder="First Name"
-          :model-value="user.firstname"
-        />
+  <div class="flex">
+    <!-- begin: User Data -->
+    <div class="w-1/2 h-full pt-20 flex flex-col items-center justify-center">
+      <!-- begin: Inputs -->
+      <div class="w-2/3">
+        <div>
+          <h2 class="py-2"> First Name </h2>
+          <BaseInput
+            placeholder="First Name"
+            :model-value="user.firstname"
+          />
+        </div>
+        <div>
+          <h2 class="py-2"> Last Name </h2>
+          <BaseInput
+            placeholder="Last Name"
+            :model-value="user.lastname"
+          />
+        </div>
+        <div>
+          <h2 class="py-2"> Username </h2>
+          <BaseInput
+            placeholder="Username"
+            :model-value="user.username"
+          />
+        </div>
+        <div>
+          <h2 class="py-2"> E-mail </h2>
+          <BaseInput
+            placeholder="E-mail"
+            :model-value="user.email"
+          />
+        </div>
+        <div>
+          <h2 class="py-2"> Password </h2>
+          <BaseInput
+            type="password"
+            placeholder="Password"
+            :model-value="user.password"
+          />
+        </div>
       </div>
-      <div>
-        <h2 class="py-2"> Last Name </h2>
-        <BaseInput
-          placeholder="Last Name"
-          :model-value="user.lastname"
-        />
+      <!-- end: Inputs -->
+      <!-- begin: Buttons -->
+      <div class="flex gap-4 pt-10">
+        <BaseButton>
+          <template #icon>
+            <Save />
+          </template>
+          <template #text>
+            Save
+          </template>
+        </BaseButton>
+        <BaseButton>
+          <template #icon>
+            <PenLine />
+          </template>
+          <template #text>
+            Edit
+          </template>
+        </BaseButton>
+        <BaseButton>
+          <template #icon>
+            <Trash2 />
+          </template>
+          <template #text>
+            Discard
+          </template>
+        </BaseButton>
       </div>
-      <div>
-        <h2 class="py-2"> Username </h2>
-        <BaseInput
-          placeholder="Username"
-          :model-value="user.username"
-        />
-      </div>
-      <div>
-        <h2 class="py-2"> E-mail </h2>
-        <BaseInput
-          placeholder="E-mail"
-          :model-value="user.email"
-        />
-      </div>
-      <div>
-        <h2 class="py-2"> Password </h2>
-        <BaseInput
-          type="password"
-          placeholder="Password"
-          :model-value="user.password"
-        />
-      </div>
+      <!-- end: Buttons -->
     </div>
-    <!-- end: Inputs -->
-    <!-- begin: Buttons -->
-    <div class="flex gap-4 py-4">
-      <BaseButton>
-        <template #icon>
-          <Save />
-        </template>
-        <template #text>
-          Save
-        </template>
-      </BaseButton>
-      <BaseButton>
-        <template #icon>
-          <PenLine />
-        </template>
-        <template #text>
-          Edit
-        </template>
-      </BaseButton>
-      <BaseButton>
-        <template #icon>
-          <Trash2 />
-        </template>
-        <template #text>
-          Discard
-        </template>
-      </BaseButton>
+    <!-- end: User Data -->
+    <div class="w-1/2 bg-amber-300">
+      TODO: FAVORITES CHARACTERS
     </div>
-    <!-- end: Buttons -->
   </div>
-  <!-- end: User Data -->
-  
 </template>
 
 <script setup>
