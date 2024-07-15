cc5ee7a5364e3dd00b1bd4925266838caa47e08d
Gustavo Henrique <gustavohenriquearagaosilva@gmail.com>
2024-07-15 11:09:07 -0300

    feat: finalizando o SelectInput

--- src/components/inputs/SelectInput.vue
+++ src/components/inputs/SelectInput.vue
@@ -1,123 +1,159 @@
 <template>
   <div 
-    class="w-fit" 
+    class="relative w-fit text-gray-800 bg-white outline outline-gray-200 rounded-md"
+    :class="[
+      visibleOptions && optionsPosition === 'below' ? 'outline-b-0 rounded-b-none' : '',
+      visibleOptions && optionsPosition === 'above' ? 'outline-t-0 rounded-t-none' : ''
+    ]"
     ref="select"
   >
+    <!-- begin: Select Input -->
     <div 
-      class="relative w-fit text-gray-800 bg-white outline outline-gray-200 rounded-md"
-      :class="visibleOptions ? 'outline-b-0 rounded-b-none' : ''"
+      class="flex w-fit"
+      @click="toggleOptions()"
     >
-      <!-- begin: Select Input -->
-      <div 
-        class="flex w-fit"
-        @click="toggleOptions()"
+      <input
+        disabled 
+        v-model="localValue"
+        class="w-full text-center bg-white rounded-md"
       >
-        <input
-          disabled 
-          v-model="localValue"
-          class="text-center bg-white rounded-md"
+      <div class="absolute right-1 rounded-md">
+        <i
+          v-if="visibleOptions"
+          class="cursor-pointer"
         >
-        <div class="absolute right-1 rounded-md">
-          <i
-            v-if="visibleOptions"
-            class="cursor-pointer"
-          >
-            <ChevronUp />
-          </i>
-          <i
-            v-else
-            class="cursor-pointer"
-          >
-            <ChevronDown />
-          </i>
-        </div>
+          <ChevronUp />
+        </i>
+        <i
+          v-else
+          class="cursor-pointer"
+        >
+          <ChevronDown />
+        </i>
       </div>
-      <!-- end: Select Input -->
-      <!-- begin: Options -->
-      <OptionsWrapper
-        :visible-options="visibleOptions"
-        :input-name="props.inputName"
-        :options="props.options"
-        @send:event="(event) => { handleInputEvent(event) }"
-      />
-      <!-- end: Options -->
     </div>
+    <!-- end: Select Input -->
+    <!-- begin: Options -->
+    <div 
+    v-if="visibleOptions" 
+    class="w-full flex flex-col text-center outline outline-gray-200 "
+    :class="[
+      'options-wrapper', optionsPosition,
+      optionsPosition === 'below' ? 'rounded-b-md' : 'rounded-t-md' 
+    ]"
+  >
+    <label
+      v-for="(option, index) in props.options"
+      :id="index"
+      class="cursor-pointer select-none odd:bg-gray-100 even:bg-white "
+      :class="optionsPosition === 'below' ? 'last:rounded-b-md' : 'first:rounded-t-md'"
+    >
+      <input
+        type="radio"
+        :id="index"
+        :name="props.inputName"
+        :value="option"
+        class="hidden"
+        @input="(event) => { handleInputEvent(event) }"
+      />
+      {{ option }}
+    </label>
+  </div>
+    <!-- end: Options -->
   </div>
 </template>
 
 <script setup>
 import { ChevronDown, ChevronUp } from 'lucide-vue-next';
-import { computed, onMounted, ref } from 'vue';
+import { onMounted, onBeforeUnmount, ref, watch, nextTick } from 'vue';
 import { capitalizeFirstLetter } from '@/utils/strings';
-import OptionsWrapper from './handler/OptionsWrapper.vue';
 
 const props = defineProps({
   modelValue: {
-		type: [String, Number],
-		default: null,
-	},
+    type: [String, Number],
+    default: null,
+  },
   options: {
     type: Array,
-    required: true
+    required: true,
   },
   inputName: {
     type: String,
-    default: 'select-input'
-  }
-})
+    default: 'select-input',
+  },
+});
 
 const emit = defineEmits(['update:model-value']);
 
 const localValue = ref(props.modelValue);
-
 const visibleOptions = ref(false);
+const optionsPosition = ref('below');
+const select = ref(null);
 
 const showOptions = () => {
   visibleOptions.value = true;
-}
+  // O nextTick garante que só vamos executar quando o DOM for totalmente atualizado
+  nextTick(() => setOptionsPosition());
+};
 
 const hideOptions = () => {
   visibleOptions.value = false;
-}
+};
 
 const toggleOptions = () => {
-  if (visibleOptions.value === true) {
+  if (visibleOptions.value) {
     hideOptions();
   } else {
     showOptions();
   }
-}
+};
 
 const handleInputEvent = (event) => {
   const value = event.target.value;
-
   localValue.value = capitalizeFirstLetter(value);
-  
   emit('update:model-value', value);
-
   hideOptions();
-}
+};
 
-const optionsPosition = ref('below');
-
-const select = ref(null);
-
-const setOptionsPosition = (select) => {
-  const selectRect = computed(() => select.value.getBoundingClientRect());
-
-  const selectHeight = selectRect.value.height;
-
-  const optionsHeight = selectHeight * props.options?.length;
-
+const setOptionsPosition = () => {
+  const selectRect = select.value.getBoundingClientRect();
+  const optionsHeight = selectRect.height * props.options.length;
   const windowHeight = window.innerHeight;
 
-  const optionsFit = ref(selectRect.value.bottom + optionsHeight < windowHeight);
-
-  optionsPosition.value = optionsFit.value ? 'below' : 'above'
-}
+  if (windowHeight - selectRect.bottom < optionsHeight && selectRect.top > optionsHeight) {
+    optionsPosition.value = 'above';
+  } else {
+    optionsPosition.value = 'below';
+  }
+};
 
 onMounted(() => {
-  setOptionsPosition(select);
-})
+  window.addEventListener('resize', setOptionsPosition);
+});
 
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', setOptionsPosition);
+});
+
+watch(visibleOptions, (newValue) => {
+  if (newValue) {
+    // O nextTick garante que só vamos executar quando o DOM for totalmente atualizado
+    nextTick(() => setOptionsPosition());
+  }
+});
 </script>
+
+<style scoped>
+.options-wrapper {
+  position: absolute;
+  z-index: 1000;
+}
+
+.options-wrapper.above {
+  bottom: 100%;
+}
+
+.options-wrapper.below {
+  top: 100%;
+}
+</style>
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="pt-10 pl-10">
-    <SelectInput :options="options" />
+  <div class="pt-10 pl-10 mx-20">
+    <SelectInput 
+      class="w-20"
+      :options="options" 
+    />
   </div>
   <div class="px-10 py-80">
     <SelectInput :options="options" />
